b1abaf5f5c9806959c6a6a080cbb7536b8b629d4
Yann Guillon <hi@yann.pro>
2024-05-31 18:57:08 +0200

    Add blog posts controller with index action

---
+++ test/controllers/blog_posts_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class BlogPostsControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get root_url
+
+    assert_response :success
+  end
+end
