99720801a84fe16a5ccd2864f22b0df73f292738
Yann Guillon <hi@yann.pro>
2024-06-04 00:40:30 +0200

    Add a show action for the BlogPostsController

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,11 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = BlogPost.all
   end
+
+  def show
+    @blog_post = BlogPost.find_by(id: params[:id])
+    if @blog_post.nil?
+      redirect_to root_url
+    end
+  end
 end
