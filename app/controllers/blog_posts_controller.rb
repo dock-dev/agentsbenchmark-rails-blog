56cea442090577fea3526744168bdb664bcb3750
Yann Guillon <hi@yann.pro>
2024-06-05 18:40:05 +0200

    Add a new action for the BlogPostsController

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -7,4 +7,23 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find_by(id: params[:id])
     redirect_to root_url unless @post
   end
+
+  def new
+    @post = BlogPost.new
+  end
+
+  def create
+    @post = BlogPost.new(post_params)
+    if @post.save
+      redirect_to blog_post_path(@post) 
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
 end
