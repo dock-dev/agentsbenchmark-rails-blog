cca15bbd9ced66446f700d944dd854ec1e91a3a7
Yann Guillon <hi@yann.pro>
2024-06-05 18:49:36 +0200

    Add an edit action for the BlogPostsController

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -12,11 +12,28 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.new
   end
 
+  def edit
+    @post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_url
+  end
+
+  def update  
+    @post = BlogPost.find(params[:id])
+    if @post.update(post_params)
+      redirect_to blog_post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity  
+    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_url
+  end
+
   def create
     @post = BlogPost.new(post_params)
     if @post.save
-      redirect_to blog_post_path(@post)
-    else 
+      redirect_to blog_post_path(@post)  
+    else
       render :new, status: :unprocessable_entity
     end
   end
@@ -24,6 +41,6 @@ class BlogPostsController < ApplicationController
   private
 
   def post_params
-    params.require(:blog_post).permit(:title, :body) 
-  end
+    params.require(:blog_post).permit(:title, :body)
+  end  
 end
