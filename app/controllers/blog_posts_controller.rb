542d29a7d11136dd618350e7cca1a92d990a9be3
Yann Guillon <hi@yann.pro>
2024-06-05 18:35:07 +0200

    Add a show action for the BlogPostsController

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,9 @@ class BlogPostsController < ApplicationController
   def index
     @posts = BlogPost.all
   end
+
+  def show
+    @post = BlogPost.find_by(id: params[:id])
+    redirect_to root_url unless @post
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "blog_posts#index"
+  
+  resources :blog_posts, only: [:index, :show]
 end
