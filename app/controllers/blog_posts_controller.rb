c56f8bd366527ef4de9380ddfc7f4d71399a79f8
yanguillon <hi@yann.pm>
2024-06-08 15:03:02 +0000

    #4 - Add new for blog posts controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,6 +2,19 @@
 
 class BlogPostsController < ApplicationController
   before_action :set_blog_post, only: [:show]
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post, notice: 'Blog post was successfully created.'
+    else
+      render :new
+    end
+  end
+end
   def index
     @blog_posts = BlogPost.all
   end
@@ -14,5 +27,8 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
   end
+end
 end
